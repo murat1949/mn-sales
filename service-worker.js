6fd97b236b4f799b52efa6bd451475d813c997d4
murat1949 <kvantor2020@gmail.com>
2026-08-04 17:43:20 +0500

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'mn-sales-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './leads.html',
+  './tasks.html',
+  './content.html',
+  './money.html',
+  './compare.html',
+  './notifications.html',
+  './manifest.webmanifest',
+  './pwa-install.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.allSettled(
+        APP_SHELL.map((url) =>
+          fetch(url, { cache: 'reload' }).then((response) => cache.put(url, response))
+        )
+      )
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request, { cache: 'reload' })
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then((cached) => cached || caches.match('./index.html'))
+      )
+  );
+});
